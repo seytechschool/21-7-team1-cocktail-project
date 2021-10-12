cf26023603bdc945f05a6a1c95c4bb66509f5703
Aizatbek <abakalbaev@gmail.com>
2021-10-13 00:10:24 +0600

    fetching data from cocktails api

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 import { cocktails } from "./data.js"; // this is your data
-import { setCoctailName } from "./drink.js";
+// import { setCoctailName } from "./drink.js";
 
 ///////////////////////////////////////////////////////////////
 ////////////////// DEVELOPE BELOW THIS LINE ///////////////////
 ///////////////////////////////////////////////////////////////
+const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 const coctailWrapper = document.querySelector(".cocktail-wrapper");
 const oopsWrapper = document.querySelector(".oops-wrapper");
+let cocktailsData;
+
+function getData(url) {
+  console.log(cocktails);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      cocktailsData = data.drinks;
+    })
+    .then(() => renderData(cocktailsData));
+}
+getData(URL);
 
 function renderData(cocktails) {
+  console.log(cocktails);
   coctailWrapper.innerHTML = "";
   for (let element of cocktails) {
+    console.log(element + "*****");
     const el = `<div class="cocktail-item"><img src="${element.strDrinkThumb}" style="width: 100%;"><p class="nameCoctail">${element.strDrink}<span class="categoryText">${element.strCategory}</span></p></div>`;
     coctailWrapper.innerHTML += el;
   }
   addEvent();
 }
-renderData(cocktails);
 
 // 2. Working with event listeners, higher order functions
 let inputField = document.querySelector(".cocktail-search-name");
@@ -23,7 +37,7 @@ inputField.addEventListener("input", () => onChange());
 
 // option
 function onChange() {
-  const newArr = cocktails.filter((item) => {
+  const newArr = cocktailsData.filter((item) => {
     const searchedCocktailName = inputField.value.toLowerCase();
     const str = item.strDrink.toLowerCase();
     const char = searchedCocktailName.toLowerCase();
@@ -45,17 +59,17 @@ function addEvent() {
 }
 
 function openCoctail() {
-  console.log("Test");
-  //   `<section class="single-drink">
-  //     <img src="https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg" class="drink-img" alt="">
-  //     <article class="drink-info">
-  //       <h2 class="drink-name">ABC</h2>
-  //       <p class="drink-desc">Layered in a shot glass.</p>
-  //       <ul class="drink-ingredients"><li><i class="far fa-check-square"></i>Amaretto</li><li><i class="far fa-check-square"></i>Baileys irish cream</li><li><i class="far fa-check-square"></i>Cognac</li></ul>
-  //       <a href="index.html" class="btn">all cocktails</a>
-  //     </article>
-  //   </section>`;
+  console.log();
 
-  // setCoctailName();
-  window.open("drink.html", "_self");
+  var winPrint = window.open("drink.html", "_self");
+  winPrint.document
+    .write(`<title>Print Report</title><section class="single-drink">
+  <img src="${element.strDrinkThumb}" class="drink-img" alt="${element.strDrink}"">
+  <article class="drink-info">
+    <h2 class="drink-name">${element.strDrink}</h2>
+    <p class="drink-desc">Layered in a shot glass.</p>
+    <ul class="drink-ingredients"><li><i class="far fa-check-square"></i>Amaretto</li><li><i class="far fa-check-square"></i>Baileys irish cream</li><li><i class="far fa-check-square"></i>Cognac</li></ul>
+    <a href="index.html" class="btn">all cocktails</a>
+  </article>
+</section>`);
 }
