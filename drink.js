addbbe3e7c3d006c84adced99f39d17647425d4d
Aizatbek <abakalbaev@gmail.com>
2021-10-16 05:05:14 +0600

    Added Bootstrap styling to drink detail page.

--- drink.js
+++ drink.js
@@ -32,9 +32,21 @@ function renderData(cocktail) {
     li += `<li class="ingredientLi">${ingredient}</li>`;
   });
 
-  const el = `<div class="left-side"><img class="drink-image" src="${item.strDrinkThumb}" alt="" /></div><div class="right-side"><h3 class="drink-name">${item.strDrink}</h3><p class="drink-receipt">${item.strInstructions}</p><ul class="drink-composition">${li}</ul><a class="link-to-index" href="index.html#search">All Coctails</a>
-  </div>`;
+  const el = `<div class="card mb-3  border-warning rounded shadow p-3 mb-5 bg-body rounded border-32 bg-light border mx-auto mt-5" style="max-width: 1000px;">
+<div class="row g-0">
+  <div class="col-md-4">
+    <img src="${item.strDrinkThumb}" class="img-fluid rounded-start" alt="..."/>
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+      <h2 class="card-title">${item.strDrink}</h2>
+      <p class="card-text text-muted">${item.strInstructions}</p>
+      <h6 class="card-subtitle mb-2 text-warning">Ingridients:</h6>
+      <ol class="list-group list-group-numbered">${li}</ol>    
+    <a href="index.html#search" class="btn btn-warning mt-3">Go Back</a>
+    </div>
+  </div>
+</div>
+</div>`;
   cocktailWrapper.innerHTML = el;
 }
-
-// localStorage.removeItem("id");
